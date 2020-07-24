7305021d3cd58fc693cc7da8913942f4080d4c15
Prajjwal Devkota <prajjwald@yahoo.com>
2020-07-23 22:54:44 -0400

    Setup automatic schedule for night-light, Launch default app selection after setup, clean up dockitem removal behavior

--- launcher.sh
+++ launcher.sh
@@ -8,3 +8,8 @@ sudo apt-get install -y ansible gpg
 echo "Running ansible"
 #no need for the -K option, as we just sudo'd twice
 ansible-playbook -i localhost, -c local -b playbooks/elementary.yml
+
+echo "######################################################################################"
+echo -e "Please setup default apps now.... \n recommended:\n\tmail: mailspring\n\tweb: brave"
+echo "######################################################################################"
+io.elementary.switchboard settings://applications/defaults 2> /dev/null
